6d2343b84470f3d1ee00f0d2b3bdc7dfabe85d82
SolidifiedRay <ray2147483647@outlook.com>
2019-12-29 15:46:08 +0800

    search button

--- zhiclasses-searchdemo/src/components/MainSearchBar/MainSearchBar.js
+++ zhiclasses-searchdemo/src/components/MainSearchBar/MainSearchBar.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './MainSearchBar.css';
 import { Link } from 'react-router-dom';
 
+
 class MainSearchBar extends React.Component{
-	
+	handleSubmit = () =>{
+		console.log(window.location);
+		window.location.href = "\search"
+	}
+
 	render(){
 		return (
 			<div className="mainSearch">
@@ -14,14 +19,12 @@ class MainSearchBar extends React.Component{
 					placeholder="课程名、课程编号、教授名"
 					onChange={this.handleChange}
 		        />
-		        <Link to='\search'>
-			        <input
-						type="submit"
-						value="搜  课"
-						className="mainSearchButton"
-						onClick={this.handleSubmit}
-			        />
-		        </Link>
+		        <input
+					type="submit"
+					value="搜  课"
+					className="mainSearchButton"
+					onClick={this.handleSubmit}
+		        />
 	  		</div>
 		);
 	}
